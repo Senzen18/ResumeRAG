921eeae9e1be03449d5f79cb4c2969574d001521
Senzen18 <sendhanaruljothi@gmail.com>
2025-02-19 20:24:38 +0530

    Fixed bugs in vectorize and pdf converter

--- load/pdf2csv.py
+++ load/pdf2csv.py
@@ -7,29 +7,29 @@ import pandas as pd
 
 CSV_PATH = '../dataset/'
 
-dir_to_read = sys.argv[1] 
-pdf_files = glob.glob(os.path.join(dir_to_read,'*.pdf'))
-resume_dict = {'ids':[],'resume':[]}
-count = 0
-extractedtext = ""
-ids = 0
-for pdf_file in pdf_files:
-    print(pdf_file)
-    pdfFileObj = open(pdf_file,'rb')               
-    pdfReader = PyPDF2.PdfReader(pdfFileObj)   
-    num_pages = len(pdfReader.pages)   
-
-    #print(num_pages)
-    extractedtext = ""
+def convert_csv(pdf_files):
+    resume_dict = {'ids':[],'resume':[]}
     count = 0
-    while count < num_pages:                       
-        pageObj = pdfReader.pages[count]
-        count +=1
-        extractedtext += pageObj.extract_text()
-    
-    resume_dict['ids'].append(ids)
-    resume_dict['resume'].append(extractedtext)
-    ids += 1
-df = pd.DataFrame(resume_dict,index=None)
-df.to_csv(CSV_PATH + 'resumes.csv',index=False)
-    
+    extractedtext = ""
+    ids = 0
+    for pdf_file in pdf_files:
+        #print(pdf_file)
+        #pdfFileObj = open(pdf_file.read(),'rb')               
+        pdfReader = PyPDF2.PdfReader(pdf_file)   
+        num_pages = len(pdfReader.pages)   
+
+        #print(num_pages)
+        extractedtext = ""
+        count = 0
+        while count < num_pages:                       
+            pageObj = pdfReader.pages[count]
+            count +=1
+            extractedtext += pageObj.extract_text()
+        
+        resume_dict['ids'].append(ids)
+        resume_dict['resume'].append(extractedtext)
+        ids += 1
+    df = pd.DataFrame(resume_dict,index=None)
+    df.to_csv(CSV_PATH + 'resumes.csv',index=False)
+    return df
+        
--- load/vectorize.py
+++ load/vectorize.py
@@ -4,18 +4,21 @@ from langchain_community.embeddings import HuggingFaceBgeEmbeddings
 from langchain_community.document_loaders import DataFrameLoader
 import pandas as pd
 
-embed = HuggingFaceBgeEmbeddings(
-    model_name = 'sentence-transformers/all-MiniLM-L6-v2',
-    model_kwargs = {'device':'cpu'}
-)
+
 text_splitter = RecursiveCharacterTextSplitter (
     chunk_size=1024,
     chunk_overlap=512,
 )
-df = pd.read_csv('../dataset/resumes.csv')
-loader = DataFrameLoader(df,page_content_column='resume')
+#df = pd.read_csv('../dataset/resumes.csv')
+def store_vectordb(df,content_column,embed_model):
+    text_splitter = RecursiveCharacterTextSplitter (
+        chunk_size=1024,
+        chunk_overlap=512,
+    )
+    loader = DataFrameLoader(df,page_content_column=content_column)
 
-documents = loader.load()
-document_chunks = text_splitter.split_documents(documents)
-vector_db = FAISS.from_documents(documents=document_chunks,embedding=embed)
-vector_db.save_local('../vectordb/')
+    documents = loader.load()
+    document_chunks = text_splitter.split_documents(documents)
+    vector_db = FAISS.from_documents(documents=document_chunks,embedding=embed_model)
+    vector_db.save_local('../vectordb/')
+    return vector_db
